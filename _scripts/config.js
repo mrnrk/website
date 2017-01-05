264d300116e044271644a48788c5f210b374a8f1
Blake Kostner <btkostner@gmail.com>
2017-01-04 23:00:33 -0700

    add a bunch of debug console logs

--- _scripts/config.js
+++ _scripts/config.js
@@ -11,16 +11,22 @@ import { url } from '~/page'
 import jQuery from '~/lib/jquery'
 
 export default jQuery.then(($) => {
+    console.log('config 1')
     const basePath = url()
+    console.log('config 2', basePath)
     const configPath = `${basePath}/api/config`
+    console.log('config 3', configPath)
 
     return new Promise((resolve, reject) => {
+        console.log('config 4')
         $.getJSON(configPath)
         .done((config) => {
+            console.log('config 5', config)
             console.log('Sitewide configuration loaded')
             return resolve(config)
         })
         .fail((jqxhr, status, err) => {
+            console.log('config 6', status, err)
             console.error(`Failed to grab sitewide configuration with ${status}`)
             console.error(err)
             return reject(err)
